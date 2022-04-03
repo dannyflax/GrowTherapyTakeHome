736d97a746dd2ed5b2ffbb131640ecc39b182798
Danny Flax <drflax29@verizon.net>
2022-04-03 19:22:02 -0400

    Add few more unit tests

--- server/tests/TestServer.py
+++ server/tests/TestServer.py
@@ -140,6 +140,91 @@ kMockDataForApi2Api3 = {
   ]
 }
 
+
+kMockInvalidDataForApi1 = {
+  "ite222ms": [
+    {
+      "project": "en.wikipedia",
+      "access": "all-access",
+      "year": "2020",
+      "month": "12",
+      "day": "29",
+      "articles": [
+        {
+          "article": "Main_Page",
+          "views": 6022145,
+          "rank": 1
+        },
+        {
+          "article": "Special:Search",
+          "views": 1205537,
+          "rank": 2
+        },
+        {
+          "article": "Bridgerton",
+          "views": 341940,
+          "rank": 3
+        },
+        {
+          "article": "Wonder_Woman_1984",
+          "views": 263432,
+          "rank": 4
+        }
+      ]
+    }
+  ]
+}
+
+kMockInvalidDataForApi2Api3 = {
+  "ite222ms": [
+    {
+      "project": "en.wikipedia",
+      "article": "Main_Page",
+      "granularity": "daily",
+      "timestamp": "2021120100",
+      "access": "all-access",
+      "agent": "all-agents",
+      "views": 12442109
+    },
+    {
+      "project": "en.wikipedia",
+      "article": "Main_Page",
+      "granularity": "daily",
+      "timestamp": "2021120200",
+      "access": "all-access",
+      "agent": "all-agents",
+      "views": 11651980
+    },
+    {
+      "project": "en.wikipedia",
+      "article": "Main_Page",
+      "granularity": "daily",
+      "timestamp": "2021120300",
+      "access": "all-access",
+      "agent": "all-agents",
+      "views": 10599348
+    },
+    {
+      "project": "en.wikipedia",
+      "article": "Main_Page",
+      "granularity": "daily",
+      "timestamp": "2021120400",
+      "access": "all-access",
+      "agent": "all-agents",
+      "views": 12824514
+    },
+    {
+      "project": "en.wikipedia",
+      "article": "Main_Page",
+      "granularity": "daily",
+      "timestamp": "2021120500",
+      "access": "all-access",
+      "agent": "all-agents",
+      "views": 12073670
+    }
+  ]
+}
+
 class MockResponse:
     __attrs__ = ['status_code', 'data']
 
@@ -157,6 +242,13 @@ class MockQueryExecutor():
         if query.startswith("/per-article/en.wikipedia.org/all-access/"):
             return MockResponse(kMockDataForApi2Api3, 200)
 
+class MockBadResponseQueryExecutor():
+    def execute(self, query):
+        if query.startswith("/top/en.wikipedia/all-access/"):
+            return MockResponse(kMockInvalidDataForApi1, 200)
+        if query.startswith("/per-article/en.wikipedia.org/all-access/"):
+            return MockResponse(kMockInvalidDataForApi2Api3, 200)
+
 class TestServerValidation(unittest.TestCase):
     def assertResultMatchesFormat(self, Result, Format):
         validationResult = ValidateNode(Result, Format)
@@ -171,6 +263,11 @@ class TestServerValidation(unittest.TestCase):
         result = Api1Week(MockQueryExecutor(), kMockInvalidWeeksParams)
         self.assertResultMatchesFormat(result, kErrorResponseFormat)
 
+    def testApi1WeekBadResponse(self):
+        result = Api1Week(MockBadResponseQueryExecutor(), kMockValidWeeksParams)
+        self.assertResultMatchesFormat(result, kErrorResponseFormat)
+
+
     def testApi1MonthSuccess(self):
         result = Api1Month(MockQueryExecutor(), kMockValidMonthsParams)
         self.assertResultMatchesFormat(result, kSuccessResponseFormat)
@@ -179,6 +276,11 @@ class TestServerValidation(unittest.TestCase):
     def testApi1MonthFail(self):
         result = Api1Month(MockQueryExecutor(), kMockInvalidMonthsParams)
         self.assertResultMatchesFormat(result, kErrorResponseFormat)
+
+    def testApi1MonthBadResponse(self):
+        result = Api1Month(MockBadResponseQueryExecutor(), kMockValidMonthsParams)
+        self.assertResultMatchesFormat(result, kErrorResponseFormat)
+
 
     def testApi2WeekSuccess(self):
         result = Api2Week(MockQueryExecutor(), kMockValidWeeksParams)
@@ -189,6 +291,11 @@ class TestServerValidation(unittest.TestCase):
         result = Api2Week(MockQueryExecutor(), kMockInvalidWeeksParams)
         self.assertResultMatchesFormat(result, kErrorResponseFormat)
 
+    def testApi2WeekBadResponse(self):
+        result = Api2Week(MockBadResponseQueryExecutor(), kMockValidWeeksParams)
+        self.assertResultMatchesFormat(result, kErrorResponseFormat)
+
+
     def testApi2MonthSuccess(self):
         result = Api2Month(MockQueryExecutor(), kMockValidMonthsParams)
         self.assertResultMatchesFormat(result, kSuccessResponseFormat)
@@ -198,6 +305,11 @@ class TestServerValidation(unittest.TestCase):
         result = Api2Month(MockQueryExecutor(), kMockInvalidMonthsParams)
         self.assertResultMatchesFormat(result, kErrorResponseFormat)
 
+    def testApi2MonthBadResponse(self):
+        result = Api2Month(MockBadResponseQueryExecutor(), kMockValidMonthsParams)
+        self.assertResultMatchesFormat(result, kErrorResponseFormat)
+
+
     def testApi3Success(self):
         result = Api3(MockQueryExecutor(), kMockValidMonthsParams)
         self.assertResultMatchesFormat(result, kSuccessResponseFormat)
@@ -205,4 +317,8 @@ class TestServerValidation(unittest.TestCase):
 
     def testApi3Fail(self):
         result = Api3(MockQueryExecutor(), kMockInvalidMonthsParams)
+        self.assertResultMatchesFormat(result, kErrorResponseFormat)
+
+    def testApi3BadResponse(self):
+        result = Api3(MockBadResponseQueryExecutor(), kMockValidMonthsParams)
         self.assertResultMatchesFormat(result, kErrorResponseFormat)
